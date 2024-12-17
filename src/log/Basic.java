a0d76793d5f16a9590ebed26002b615de8dee596
kritikrishnan03 <kritikrishnan03@gmail.com>
2024-12-17 14:30:38 +0530

    First commit

---
+++ src/log/Basic.java
@@ -0,0 +1,16 @@
+package log;
+
+
+
+public class Basic {
+	public void shapes() {
+		System.out.println("Circle");
+		
+		
+	}
+public static void main(String[] args) {
+	//class name obj name = new Class name();
+	Basic b = new Basic();
+	b.shapes();
+}
+}
